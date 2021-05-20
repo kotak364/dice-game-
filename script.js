70883fa62d1574d2456d90030fb9dc1a503a50ec
kotak364 <75615832+kotak364@users.noreply.github.com>
2021-05-19 16:29:23 -0800

    update

--- script.js
+++ script.js
@@ -1,50 +1,28 @@
-// console.log(randomnumber);
 
-// console.log(a);
+//  Normal js code 
+
 const b = document.querySelector(".btn");
 
+b.addEventListener("click", function () {
+  const randomnumber = Math.floor(Math.random() * 6 + 1);
+  const a = "dice" + randomnumber + ".png";
 
-b.addEventListener("click" ,function() {
-    
-const randomnumber = Math.floor((Math.random() * 6) + 1);
-const a = "dice"+randomnumber+".png";
+  const randomnumber2 = Math.floor(Math.random() * 6 + 1);
 
+  const c = "dice" + randomnumber2 + ".png";
 
-const randomnumber2 = Math.floor((Math.random() * 6) + 1);
+  document.getElementById("player1").setAttribute("src", a);
 
-const c = "dice"+randomnumber2+".png";
+  document.getElementById("player2").setAttribute("src", c);
 
-     document.getElementById("player1").setAttribute('src', a
-        );
-
-        document.getElementById("player2").setAttribute('src', c
-        );
-
-        if (randomnumber>randomnumber2) {
-                document.querySelector("h1").textContent = "Player 1 Won";
-                
-        }
-        else if (randomnumber<randomnumber2){
-                document.querySelector("h1").textContent = "Player 2 Won";
-
-        }
-        else
-        {
-                document.querySelector("h1").textContent = "Draw";
-
-        }
-
-})
+  if (randomnumber > randomnumber2) {
+    document.querySelector("h1").textContent = "Player 1 Won";
+  } else if (randomnumber < randomnumber2) {
+    document.querySelector("h1").textContent = "Player 2 Won";
+  } else {
+    document.querySelector("h1").textContent = "Draw";
+  }
+});
 
 
-
-
-
-
-
-
-    
-
-
-
-
+//  ecma script 6  js code 
